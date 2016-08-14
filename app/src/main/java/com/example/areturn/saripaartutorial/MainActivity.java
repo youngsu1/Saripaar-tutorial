dc96b39ab08121a1b1be8f7d3462f854469ad9d0
노영수 <guitaryc33@gmail.com>
2016-08-14 11:30:33 +0900

    first

--- app/src/main/java/com/example/areturn/saripaartutorial/MainActivity.java
+++ app/src/main/java/com/example/areturn/saripaartutorial/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.areturn.saripaartutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.username)
+    EditText username;
+    @BindView(R.id.password)
+    EditText password;
+    @BindView(R.id.passwordConfirm)
+    EditText passwordConfirm;
+    @BindView(R.id.email)
+    EditText email;
+    @BindView(R.id.mobile)
+    EditText mobile;
+    @BindView(R.id.submitButton)
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick(R.id.submitButton)
+    public void onClick() {
+
     }
 }
